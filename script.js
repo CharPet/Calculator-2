28815e0f95d6f83864b5375755c719ca20093cff
Chameleon <chameleon@Xubuntu.myguest.virtualbox.org>
2023-08-11 18:39:32 +0300

    Rewriten the code (task 6)

--- script.js
+++ script.js
@@ -1,90 +1,91 @@
 let firstNumber = null;
-let secondNumber = null;
 let operator = null;
-let displayValue = null;
+let secondNumber = null;
+let displayValue = '0';
 
- 
-function add(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(firstNumber, secondNumber) {
-    return firstNumber / secondNumber;
+function divide(a, b) {
+    if (b === 0) {
+        return 'Error';
+    }
+    return a / b;
 }
 
-function operate(operator, firstNumber, secondNumber) {
-    firstNumber = parseFloat(firstNumber);
-    secondNumber = parseFloat(secondNumber);
-
-    if (operator === "+") {
-        return add(firstNumber, secondNumber);
-    } else if (operator === "-") {
-        return subtract(firstNumber, secondNumber);
-    } else if (operator === "*") {
-        return multiply(firstNumber, secondNumber);
-    } else if (operator === "/") {
-        return divide(firstNumber, secondNumber);
+function operate(a, op, b) {
+    switch(op) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);    
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);    
     }
 }
 
-const digitButtons = document.querySelectorAll('.digit-buttons .btn');
-digitButtons.forEach(digitButton => {
-    digitButton.addEventListener('click', () => {
-        updateLowerDisplay(digitButton.textContent);
+const lowerDisplay = document.querySelector('.lower-display');
+
+const numberButtons = document.querySelectorAll('.number-buttons .btn');
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const clickedNumber = button.textContent;
+
+        if (operator === null) {
+            if (displayValue === '0') {
+                displayValue = clickedNumber;
+            } else {
+                displayValue += clickedNumber;
+            }
+        } else {
+            displayValue += clickedNumber;
+            secondNumber = parseFloat(displayValue);
+        }
+        lowerDisplay.textContent = displayValue;
     });
 });
 
 const operatorButtons = document.querySelectorAll('.operator-buttons .btn');
 operatorButtons.forEach(operatorButton => {
     operatorButton.addEventListener('click', () => {
-        firstNumber = displayValue; 
-        operator = operatorButton.textContent;
-        displayValue = ''; 
-        updateLowerDisplay(firstNumber);
-        updateUpperDisplay(firstNumber, operator);
+        if (firstNumber === null) {
+            firstNumber = parseFloat(displayValue);
+            operator = operatorButton.textContent;
+            displayValue = `${firstNumber} ${operator} `;
+        } else if (firstNumber !== null && operator !== null && secondNumber === null) {
+            operator = operatorButton.textContent;
+            displayValue = `${firstNumber} ${operator} `;
+        } else if (firstNumber !== null && operator !== null && secondNumber !== null) {
+            secondNumber = parseFloat(displayValue);
+            const result = operate(firstNumber, operator, secondNumber);
+            displayValue = result.toString();
+            firstNumber = result;
+            secondNumber = null;
+            operator = operatorButton.textContent;
+        }
+        lowerDisplay.textContent = displayValue;
     });
 });
 
-const updateLowerDisplay = (value) => {
-    const lowerDisplay = document.querySelector('.lower-display');
-    if (lowerDisplay.textContent === '0' || displayValue === '0') {
-        displayValue = value;
-    } else {
-        displayValue += value;
-    }
-    lowerDisplay.textContent = displayValue;
-}
-
-const updateUpperDisplay = (firstNumber, operator) => {
-    const upperDisplay = document.querySelector('.upper-display');
-    upperDisplay.innerHTML = `${firstNumber} ${operator}`;
-}
-
-
-const clearButtonInput = () => {
-    const clearButton = document.querySelector('.special-btn#clear');
-    clearButton.addEventListener('click', () => {
-        firstNumber = null;
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', () => {
+    if (firstNumber !== null && operator !== null && secondNumber !== null) {
+        const result = operate(firstNumber, operator, secondNumber);
+        lowerDisplay.textContent = result;
+        displayValue = result.toString();
+        firstNumber = result;
         operator = null;
         secondNumber = null;
-        displayValue = '0';
-        updateLowerDisplay(displayValue);
-        updateUpperDisplay('&nbsp;', '&nbsp;');
-    });
-}
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    
-    clearButtonInput();
+    }
 });
-
